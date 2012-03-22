2dd9fb06d1955fe9c4668c6467b56bf5c1df41f1
Andre Pankratz <andre@webwarelab.com>
2012-03-22 22:53:11 +0100

    Spec a mov video

--- spec/vidibus/fileinfo/processor/video_spec.rb
+++ spec/vidibus/fileinfo/processor/video_spec.rb
@@ -673,6 +673,45 @@ describe Vidibus::Fileinfo::Processor::Video do
       end
     end
 
+    context 'of a mov video' do
+      before do
+        stub(subject).process_cmd { results['mov'] }
+        @metadata = subject.data
+      end
+
+      it 'should extract the audio codec' do
+        @metadata[:audio_codec].should eq('aac')
+      end
+
+      it 'should extract the audio sample rate' do
+        @metadata[:audio_sample_rate].should eq(48000)
+      end
+
+      it 'should extract the bitrate' do
+        @metadata[:bitrate].should eq(3342)
+      end
+
+      it 'should extract the duration in seconds' do
+        @metadata[:duration].should eq(596.48)
+      end
+
+      it 'should extract frames per second' do
+        @metadata[:fps].should eq(24.0)
+      end
+
+      it 'should extract the height' do
+        @metadata[:height].should eq(480)
+      end
+
+      it 'should extract the video codec' do
+        @metadata[:video_codec].should eq('h264 (Main)')
+      end
+
+      it 'should extract the width' do
+        @metadata[:width].should eq(854)
+      end
+    end
+
     it "should raise DataError if metadata is invalid" do
       stub(subject).process_cmd {results["invalid_data"]}
       expect {subject.data}.to raise_error(Vidibus::Fileinfo::DataError)
