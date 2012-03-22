7dd03cc1f9cb3f1e8afbfb896d4150ed27cceaa2
Andre Pankratz <andre@webwarelab.com>
2012-03-22 20:04:42 +0100

    Spec a mkv video without bitrate

--- spec/vidibus/fileinfo/processor/video_spec.rb
+++ spec/vidibus/fileinfo/processor/video_spec.rb
@@ -361,6 +361,45 @@ describe Vidibus::Fileinfo::Processor::Video do
       end
     end
 
+    context 'of a mkv video without bitrate' do
+      before do
+        stub(subject).process_cmd { results['mkv_without_bitrate'] }
+        @metadata = subject.data
+      end
+
+      it 'should extract the audio codec' do
+        @metadata[:audio_codec].should eq('aac')
+      end
+
+      it 'should extract the audio sample rate' do
+        @metadata[:audio_sample_rate].should eq(48000)
+      end
+
+      it 'should not extract the bitrate' do
+        @metadata[:bitrate].should be_nil
+      end
+
+      it 'should extract the duration in seconds' do
+        @metadata[:duration].should eq(44.58)
+      end
+
+      it 'should extract frames per second' do
+        @metadata[:fps].should eq(23.98)
+      end
+
+      it 'should extract the height' do
+        @metadata[:height].should eq(256)
+      end
+
+      it 'should extract the video codec' do
+        @metadata[:video_codec].should eq('h264 (High)')
+      end
+
+      it 'should extract the width' do
+        @metadata[:width].should eq(592)
+      end
+    end
+
     it "should raise DataError if metadata is invalid" do
       stub(subject).process_cmd {results["invalid_data"]}
       expect {subject.data}.to raise_error(Vidibus::Fileinfo::DataError)
