97beb393d45741fccba3d99343d507ea9b5bdfca
Andre Pankratz <andre@webwarelab.com>
2012-03-22 20:56:58 +0100

    Spec a ivf video without audio track

--- spec/vidibus/fileinfo/processor/video_spec.rb
+++ spec/vidibus/fileinfo/processor/video_spec.rb
@@ -634,6 +634,45 @@ describe Vidibus::Fileinfo::Processor::Video do
       end
     end
 
+    context 'of an ivf video without audio track' do
+      before do
+        stub(subject).process_cmd { results['ivf_without_audio'] }
+        @metadata = subject.data
+      end
+
+      it 'should not extract the audio codec' do
+        @metadata[:audio_codec].should be_nil
+      end
+
+      it 'should not extract the audio sample rate' do
+        @metadata[:audio_sample_rate].should be_nil
+      end
+
+      it 'should extract the bitrate' do
+        @metadata[:bitrate].should eq(13983)
+      end
+
+      it 'should extract the duration in seconds' do
+        @metadata[:duration].should eq(10.0)
+      end
+
+      it 'should extract frames per second' do
+        @metadata[:fps].should eq(50.0)
+      end
+
+      it 'should extract the height' do
+        @metadata[:height].should eq(1080)
+      end
+
+      it 'should extract the video codec' do
+        @metadata[:video_codec].should eq('vp8')
+      end
+
+      it 'should extract the width' do
+        @metadata[:width].should eq(1920)
+      end
+    end
+
     it "should raise DataError if metadata is invalid" do
       stub(subject).process_cmd {results["invalid_data"]}
       expect {subject.data}.to raise_error(Vidibus::Fileinfo::DataError)
