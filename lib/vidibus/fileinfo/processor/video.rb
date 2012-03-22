1fe6e3fada3e8deb80708842ab905d4f770871ce
Andre Pankratz <andre@webwarelab.com>
2012-03-22 20:50:14 +0100

    Improve extraction of audio codec

--- lib/vidibus/fileinfo/processor/video.rb
+++ lib/vidibus/fileinfo/processor/video.rb
@@ -21,7 +21,7 @@ module Vidibus
         protected
 
         def audio_codec
-          @raw_metadata[/Audio:\s+([a-z0-9]+),/, 1]
+          @raw_metadata[/Audio:\s+([a-z0-9_]+),/, 1]
         end
 
         def audio_sample_rate
