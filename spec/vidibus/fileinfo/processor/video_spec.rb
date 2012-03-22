f034e0fe0b4804a3406545fe438f6bb216e42fc6
Andre Pankratz <andre@webwarelab.com>
2012-03-22 19:06:01 +0100

    Spec wmv data

--- spec/vidibus/fileinfo/processor/video_spec.rb
+++ spec/vidibus/fileinfo/processor/video_spec.rb
@@ -244,6 +244,45 @@ describe Vidibus::Fileinfo::Processor::Video do
       end
     end
 
+    context 'of a wmv video' do
+      before do
+        stub(subject).process_cmd { results['wmv'] }
+        @metadata = subject.data
+      end
+
+      it 'should extract the audio codec' do
+        @metadata[:audio_codec].should eq('wmav2')
+      end
+
+      it 'should extract the audio sample rate' do
+        @metadata[:audio_sample_rate].should eq(44100)
+      end
+
+      it 'should extract the bitrate' do
+        @metadata[:bitrate].should eq(699)
+      end
+
+      it 'should extract the duration in seconds' do
+        @metadata[:duration].should eq(151.79)
+      end
+
+      it 'should extract frames per second' do
+        @metadata[:fps].should eq(29.97)
+      end
+
+      it 'should extract the height' do
+        @metadata[:height].should eq(300)
+      end
+
+      it 'should extract the video codec' do
+        @metadata[:video_codec].should eq('wmv3 (Main)')
+      end
+
+      it 'should extract the width' do
+        @metadata[:width].should eq(400)
+      end
+    end
+
     it "should raise DataError if metadata is invalid" do
       stub(subject).process_cmd {results["invalid_data"]}
       expect {subject.data}.to raise_error(Vidibus::Fileinfo::DataError)
